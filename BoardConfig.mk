6c1254babf6f05eedfae570111363ba6d17db154
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 19:56:16 -0700

    hanoip: Define paths to DTC and MKDTIMG
    These are no longer on PATH, so we have to define them explicitly
    for the kernel make system to find them.
    
    Change-Id: I69f1a01f2f3d4c90e5a2337f44ee8e1bcb1cc1e8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,9 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 TARGET_KERNEL_SOURCE := kernel/motorola/sm6150
 TARGET_KERNEL_CONFIG := vendor/hanoip_defconfig
+TARGET_KERNEL_ADDITIONAL_FLAGS += \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 # TARGET_PREBUILT_DTB := $(PLATFORM_PATH)/prebuilt/dtb.img
