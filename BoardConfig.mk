59b9203031b81da2891b59652bca099c9a4eacb3
Andrew Hexen <SyberHexen@gmail.com>
2021-07-01 12:15:52 -0700

    hanoip: dtc: specify we are using prebuilt.
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_SOURCE := kernel/motorola/sm6150
 TARGET_KERNEL_CONFIG := vendor/hanoip_defconfig
 TARGET_KERNEL_ADDITIONAL_FLAGS += \
+    DTC_PREBUILT=true \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
     DTC_OVERLAY_TEST_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/ufdt_apply_overlay \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
