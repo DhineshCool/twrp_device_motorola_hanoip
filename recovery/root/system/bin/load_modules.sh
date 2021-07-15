8c58b712bb182a277f736016af69dd9ae85bb726
Naxo79 <nacho79.garcia@gmail.com>
2021-07-15 01:36:32 -0400

    odessa: Import touchsreen modules

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -31,9 +31,9 @@ wait_for_poweron()
 }
 
 # Load all needed modules
+insmod $module_path/bq2597x_mmi.ko
 insmod $module_path/sensors_class.ko
 insmod $module_path/fpc1020_mmi.ko
-insmod $module_path/goodix_fod_mmi.ko
 insmod $module_path/utags.ko
 insmod $module_path/exfat.ko
 insmod $module_path/mmi_annotate.ko
@@ -42,14 +42,14 @@ insmod $module_path/mmi_sys_temp.ko
 insmod $module_path/moto_f_usbnet.ko
 insmod $module_path/snd_smartpa_aw882xx.ko
 insmod $module_path/focaltech_0flash_mmi.ko
+insmod $module_path/nova_0flash_mmi.ko
 
 cd $firmware_path
 touch_product_string=$(ls $touch_class_path)
-insmod $module_path/aw8646.ko
-firmware_file="focaltech-ft8756-0d-0000-liber.bin"
+firmware_file="focaltech-txd-ft8756-06-0000-odessa.bin"
 
 
-touch_path=/sys$(cat $touch_class_path/$touch_product_string/path | awk -Fliber '{print $1}')
+touch_path=/sys$(cat $touch_class_path/$touch_product_string/path | awk -Fodessa '{print $1}')
 wait_for_poweron
 echo $firmware_file > $touch_path/doreflash
 echo 1 > $touch_path/forcereflash
