3d408552094fa00a55d4c835e644616832869a46
DhineshCool <dhineshcool585@gmail.com>
2022-02-08 22:43:11 +0530

    hanoip : dont inline libandroidicu

--- device.mk
+++ device.mk
@@ -102,7 +102,3 @@ PRODUCT_SOONG_NAMESPACES += \
 # OEM otacert
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     $(LOCAL_PATH)/security/ota
-
-# Apex libraries
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
