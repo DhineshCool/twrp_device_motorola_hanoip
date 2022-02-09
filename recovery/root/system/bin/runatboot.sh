3f42f22363df6931d2245f6e221ae3a11a4f45d4
Siddhesh <skoyande17@gmail.com>
2022-02-09 15:47:22 +0530

    hanoip: load modules on init and cleanup script
    Signed-off-by: DhineshCool <dhineshcool585@gmail.com>

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -7,28 +7,6 @@ touch_path=
 firmware_path=/vendor/firmware
 firmware_file=
 
-wait_for_poweron()
-{
-	local wait_nomore
-	local readiness
-	local count
-	wait_nomore=60
-	count=0
-	while true; do
-		readiness=$(cat $touch_path/poweron)
-		if [ "$readiness" == "1" ]; then
-			break;
-		fi
-		count=$((count+1))
-		[ $count -eq $wait_nomore ] && break
-		sleep 1
-	done
-	if [ $count -eq $wait_nomore ]; then
-		return 1
-	fi
-	return 0
-}
-
 # Load TouchScreen Modules
 insmod $module_path/exfat.ko
 insmod $module_path/ilitek_v3_mmi.ko
@@ -42,7 +20,6 @@ insmod $module_path/mmi_sys_temp.ko
 insmod $module_path/mpq-adapter.ko
 insmod $module_path/sensors_class.ko
 insmod $module_path/utags.ko
-
 
 cd $firmware_path
 touch_product_string=$(ls $touch_class_path)
